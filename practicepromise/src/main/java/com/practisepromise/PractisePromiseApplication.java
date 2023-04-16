0b12c1f8b97d6c6ff5b120abff659967730442b9
Mihael <mihael.belko@windowslive.com>
2023-04-16 14:19:09 +0200

    Added project

---
+++ practicepromise/src/main/java/com/practisepromise/PractisePromiseApplication.java
@@ -0,0 +1,13 @@
+package com.practisepromise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PractisePromiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PractisePromiseApplication.class, args);
+	}
+
+}
---
+++ practicepromise/src/test/java/com/practisepromise/PractisePromiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practisepromise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PractisePromiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
